7dabc1b1da1b1be00a3a518d98b82b6fb94e888f
shizheng <shizheng89@qq.com>
2019-04-24 18:10:32 +0800

    commit

--- WFGenerator/Program.cs
+++ WFGenerator/Program.cs
@@ -6,6 +6,12 @@ using System.Windows.Forms;
 
 namespace WFGenerator
 {
+
+    /*
+     C:\Windows\Microsoft.NET\Framework\v4.0.30319\Config
+ 
+    <add name="DefaultSqlite" connectionString="Data Source=172.18.132.141;port=3306;Initial Catalog=DefaultSqlite;uid=root;password=123456;Charset=utf8" providerName="MySql.Data.MySqlClient"/>
+   */
     static class Program
     {
         /// <summary>
